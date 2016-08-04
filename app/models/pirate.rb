dabb43abe0cb587e47ec6326cb34bed5220dd094
aehmt <calisahmet@gmail.com>
2016-08-04 12:59:38 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,27 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  attr_reader :ships
+
+  @@all = []
+
+  def initialize(attr)
+    attr.each {|key, value| self.send(("#{key.to_s}="), value)}
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def ships=(ships)
+    @ships = ships.map do |ship|
+       new_ship = Ship.new(ship)
+       new_ship.pirate = self
+       new_ship
+     end 
+  end 
+
+  def self.clear
+    @@all.clear
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+  attr_accessor :name, :type, :booty, :pirate
+  @@all = []
+
+  def initialize(attr)
+    attr.each {|key, value| self.send(("#{key.to_s}="), value)}
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
 end
